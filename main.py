c5d3f056412b182abbe05f435cf3357394572505
minusZeroo <pcijideani@gmail.com>
2022-02-20 13:06:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#%%
+from pathlib import Path
+
+import pandas as pd
+
+titanic_df = pd.read_csv(Path().joinpath('data','titanic.csv'))
+
+#%%
+sample_titanic=titanic_df.sample(10)
+
+#%%
+sum_null = titanic_df.isnull().sum()
+
+#%%
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+def count_plot(x, hue, data):
+    sns.countplot(x=x, hue=hue, data=data)
+    plt.show()
+
+count_plot('Survived', 'Sex', titanic_df)
+
+#%%
+sns.countplot(x='Survived', hue='Pclass', data=titanic_df)
+plt.show()
